42980e375a5b33eba66ebc29afc77297b3a28308
chids <guyepic39@gmail.com>
2025-11-06 00:45:37 +0000

    added sig verfication

--- src/signer.h
+++ src/signer.h
@@ -29,10 +29,8 @@ Signer* new_signer(OSSL_LIB_CTX *ctx) {
     return signer;
 }
 
-EthSig *sign_data(Signer *signer, const char* data, size_t data_len) {
-    size_t sig_len;
-    int ret = 0, public =0;
 
+EthSig *sign_data(Signer *signer, const char* data, size_t data_len) {
     EthSig *sig = malloc(sizeof(EthSig));
 
     EVP_MD_CTX *sign_context = EVP_MD_CTX_new();
@@ -56,18 +54,18 @@ EthSig *sign_data(Signer *signer, const char* data, size_t data_len) {
         goto cleanup;
     }
 
-    if(&sig->len <= 0) {
+    if(sig->len <= 0) {
         fprintf(stderr, "EVP_DigestSignFinal retunred invalid signature length\n");
         goto cleanup;
     }
 
 
-    sig->bytes = OPENSSL_malloc(sig_len);
+    sig->bytes = OPENSSL_malloc(sig->len);
     if (sig->bytes == NULL) {
         fprintf(stderr, "sign error(): No memory to allocate signature");
     }
 
-    if(!EVP_DigestSignFinal(sign_context, sig->bytes, &sig_len)) {
+    if(!EVP_DigestSignFinal(sign_context, sig->bytes, &sig->len)) {
         fprintf(stderr, "sign() EVP_DigestSignFinal failed to finalise signature\n");
         goto cleanup;
     }
@@ -78,9 +76,45 @@ EthSig *sign_data(Signer *signer, const char* data, size_t data_len) {
         if(sig->bytes) {
             OPENSSL_free(sig->bytes);
         }
-
         free(sig);
-
         EVP_MD_CTX_free(sign_context);
         return NULL;
+}
+
+int verify_sig(OSSL_LIB_CTX *libctx, EVP_PKEY* pub_key, EthSig *sig,
+    const unsigned char *data, size_t data_len)
+{
+
+    int ret = 0;
+    EVP_MD_CTX *verify_context = EVP_MD_CTX_new();
+
+    if (verify_context == NULL) {
+        fprintf(stderr, "EVP_MD_CTX_new failed.\n");
+        goto cleanup;
+    }
+
+    // verify
+    if (!EVP_DigestVerifyInit_ex(verify_context, NULL, SIG_NAME, libctx, NULL, pub_key, NULL)) {
+        fprintf(stderr, "EVP_DigestVerifyInit failed.\n");
+        goto cleanup;
+    }
+
+    if (!EVP_DigestVerifyUpdate(verify_context, data, data_len)) {
+        fprintf(stderr, "EVP_DigestVerifyUpdate(hamlet_1) failed.\n");
+        goto cleanup;
+    }
+
+    if (EVP_DigestVerifyFinal(verify_context, sig->bytes
+        , sig->len) <= 0) {
+        fprintf(stderr, "EVP_DigestVerifyFinal failed.\n");
+        goto cleanup;
+    }
+    fprintf(stdout, "Signature verified.\n");
+    ret = 1;
+
+cleanup:
+    /* OpenSSL free functions will ignore NULL arguments */
+    EVP_PKEY_free(pub_key);
+    EVP_MD_CTX_free(verify_context);
+    return ret;
 }
